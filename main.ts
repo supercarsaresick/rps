a03eb8a3311020666bdc0faf9a1343187c6dc430
supercarsaresick <122555323+supercarsaresick@users.noreply.github.com>
2023-01-30 12:18:37 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+function updateScores () {
+    OLED.clear()
+    OLED.writeStringNewLine("Player A:" + PA)
+    OLED.newLine()
+    OLED.writeStringNewLine("Player B:" + PB)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties:" + Ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds:" + Rounds)
+}
+let Rounds = 0
+let Ties = 0
+let PB = 0
+let PA = 0
+OLED.init(128, 64)
+PA = 0
+PB = 0
+Ties = 0
+Rounds = 0
+OLED.writeStringNewLine("LETS PLAY ROCK PAPER SCISSORS! :D")
+basic.pause(2000)
+updateScores()
