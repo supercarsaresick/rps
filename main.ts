bc6be7201af5f921156a6d6fe84ea669575ad00e
supercarsaresick <122555323+supercarsaresick@users.noreply.github.com>
2023-01-31 11:56:41 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,42 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    PA += 1
+    Rounds += 1
+    updateScores()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    Ties += 1
+    Rounds += 1
+    updateScores()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # # . .
+        `)
+    PB += 1
+    Rounds += 1
+    updateScores()
+})
+input.onGesture(Gesture.Shake, function () {
+    startReset()
+})
 function updateScores () {
     OLED.clear()
     OLED.writeStringNewLine("Player A:" + PA)
@@ -8,15 +47,18 @@ function updateScores () {
     OLED.newLine()
     OLED.writeStringNewLine("Rounds:" + Rounds)
 }
-let Rounds = 0
-let Ties = 0
+function startReset () {
+    OLED.init(128, 64)
+    PA = 0
+    PB = 0
+    Ties = 0
+    Rounds = 0
+    OLED.writeStringNewLine("LETS PLAY ROCK PAPER SCISSORS! :D")
+    basic.pause(2000)
+    updateScores()
+}
 let PB = 0
+let Ties = 0
+let Rounds = 0
 let PA = 0
-OLED.init(128, 64)
-PA = 0
-PB = 0
-Ties = 0
-Rounds = 0
-OLED.writeStringNewLine("LETS PLAY ROCK PAPER SCISSORS! :D")
-basic.pause(2000)
-updateScores()
+startReset()
